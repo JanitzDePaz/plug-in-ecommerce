1f05fbe0a0c2fc8b019204df40d56942031f77cb
Janitz <janitzdp@gmail.com>
2026-08-18 03:16:15 +0200

    feat(cart): add process purchase button in CartMenu

--- client/src/components/layout/header/CartMenu.tsx
+++ client/src/components/layout/header/CartMenu.tsx
@@ -4,9 +4,10 @@ import { Show, SignInButton, useUser } from "@clerk/react";
 import { useEffect } from "react";
 import { useCart } from "src/api/useCart";
 import trashIcon from "../../../assets/icons/shop/TrashIcon.svg";
+import { ShopButtons } from "src/components/buttons/ShopButtons";
 
 export const CartMenu = () => {
-  const { cartMenu, cartProducts, setCartProducts } = cartStorage();
+  const { cartMenu, cartProducts, setCartProducts, toggleCart} = cartStorage();
   const { getProducts, delProduct } = useCart();
   const { user, isLoaded } = useUser();
   useEffect(() => {
@@ -49,9 +50,7 @@ export const CartMenu = () => {
             <p>El carrito aún esta vacio</p>
           ) : (
             <>
-              {cartProducts.map((prod) => {
-                console.log(prod);
-                return (
+              {cartProducts.map((prod) => (
                   <article
                     key={prod.name}
                     className="flex gap-5 rounded-lg h-30"
@@ -77,18 +76,20 @@ export const CartMenu = () => {
                       </button>
                     </div>
                   </article>
-                );
-              })}
+                ))}
               <div className="border-t border-dashed border-gray-400 py-5 px-10">
                 <div className="flex justify-between">
                   <h3>Unidades:</h3>
                   <h3>{totalAmount}</h3>
                 </div>
-
                 <div className="flex justify-between">
                   <h2>Precio total:</h2>
                   <h2 className="font-semibold text-xl">{totalPrice.toFixed(2)}€</h2>
                 </div>
+                <div className="full flex justify-center pt-2">
+                   <button className="py-2 px-4 text-white text-lg bg-blue-500 rounded-lg hover:bg-blue-600 duration-100" onClick={() => toggleCart()}>Procesar compra</button>
+                </div>
+                
               </div>
             </>
           )}
