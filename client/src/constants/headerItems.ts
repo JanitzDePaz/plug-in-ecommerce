3c8530daa8762a0ef5ead543ca58cd15f4a10ba8
Janitz <janitzdp@gmail.com>
2026-08-19 15:31:22 +0200

    fix(headerItems): update header items to remove 'Novedades' and 'Acerca de'

--- client/src/constants/headerItems.ts
+++ client/src/constants/headerItems.ts
@@ -2,6 +2,5 @@ export const headerItems = [
     { id: 1, label: "Inicio", to: "/" },
     { id: 2, label: "Tienda", to: "/tienda" },
     { id: 3, label: "Contacto", to: "/contacto" },
-    { id: 4, label: "Novedades", to: "/novedades" },
-    { id: 5, label: "Acerca de", to: "/acerca-de" } 
+    { id: 4, label: "Soporte", to: "/soporte" }
 ];
